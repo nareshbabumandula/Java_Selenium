cbb92615457c288abb88798a2a7d79c60080d001
Mandula Naresh Babu <naresh2223@gmail.com>
2019-10-03 14:42:58 -0400

    03-Oct-2019:Naresh: Added shortcut for code commenting

--- src/basics/Shortcuts.java
+++ src/basics/Shortcuts.java
@@ -3,7 +3,13 @@ package basics;
 public class Shortcuts {
 
 	public static void main(String[] args) {
+		
+		//sysout + Ctrl + Spacebar  - System.out.println();
 		System.out.println("Welcome to the core java training for Selenium...!");
+		
+		// To comment a line of code used - //
+		int a=100;
+		
 
 	}
 
