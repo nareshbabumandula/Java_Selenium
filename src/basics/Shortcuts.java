6be645f025bce20c76691d8ef629d638ef27120a
Himanagothi <himabindu.nagothi@gmail.com>
2019-10-07 14:00:44 -0400

    10/07/2019:Himabindu: Added shortcuts for search and replace to the Shortcuts.java

--- src/basics/Shortcuts.java
+++ src/basics/Shortcuts.java
@@ -3,20 +3,31 @@ package basics;
 public class Shortcuts {
 
 	public static void main(String[] args) {
-		
+
 		//sysout + Ctrl + Spacebar  - System.out.println();
 		System.out.println("Welcome to the core java training for Selenium...!");
-		
+
 		// To comment a line of code used - //
 		int a=100;
-		
+
 		// Run the java code
 		//1. Click on the Run button in the toolbar
 		//2. Ctrl + F11
 		//3. Right Click on Code Editor -> Run As -> Java Application
-		
-		
 
+		/*Ctrl+F	Open find and replace dialog
+		Ctrl+K/Ctrl+Shift+K	Find previous / find next occurrence of search term (close find window first)
+		Ctrl+H	Search Workspace (Java Search, Task Search, and File Search)
+		Ctrl+J/Ctrl+Shift+J	Incremental search forward / backwards. Type search term after pressing ctrl+j, there is now search window
+		Ctrl+Shift+O	Open a resource search dialog to find any class
+		 Indentions and Comments
+		Tab/Shift+Tab	Increase / decrease indent of selected text
+		Ctrl+I	Correct indention of selected text or of current line
+		Ctrl+Shift+F	Autoformat all code in Editor using code formatter
+		Ctrl+/	Comment / uncomment line or selection ( adds '//' )
+		Ctrl+Shift+\	Remove Block Comment
+		
+		*/
 	}
 
 }
