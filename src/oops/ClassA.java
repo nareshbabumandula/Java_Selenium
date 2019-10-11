85499110e1459ca94e9b4ade9ff73ed19459a53d
shilpa615 <56131156+shilpa615@users.noreply.github.com>
2019-10-11 14:29:22 -0400

    10/10/2019:Shilpa: Class, object and inheritance

---
+++ src/oops/ClassA.java
@@ -0,0 +1,44 @@
+package oops;
+
+public class ClassA {
+
+	int a = 10;
+	int b = 20;
+
+	public void addition() {
+		int c = a + b;
+		System.out.println("Addition of a and b is : " + c);
+	}
+
+	public void subtraction() {
+		int c = a - b;
+		System.out.println("Subtraction of a and b is : " + c);
+	}
+
+	public void multiplication() {
+		int c = a * b;
+		System.out.println("Multiplication of a and b is : " + c);
+	}
+
+	public void division() {
+		int c = a / b;
+		System.out.println("Division of a and b is : " + c);
+	}
+
+	public void modulus() {
+		int c = a % b;
+		System.out.println("Modulus of a and b is : " + c);
+	}
+
+	public static void main(String[] args) {
+		ClassA obj = new ClassA();
+		System.out.println(obj.a);
+		System.out.println(obj.b);
+		obj.addition();
+		obj.subtraction();
+		obj.division();
+		obj.modulus();
+		obj.multiplication();
+	}
+
+}
---
+++ src/oops/ClassB.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class ClassB extends ClassA {
+
+	public static void main(String[] args) {
+		ClassB bb = new ClassB();
+		bb.addition();
+
+	}
+
+}
