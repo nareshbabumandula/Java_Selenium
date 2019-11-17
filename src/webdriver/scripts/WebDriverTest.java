6df4b5e0e6e00107b02d1165267e5d94d48cf3c3
Mandula Naresh Babu <naresh2223@gmail.com>
2019-11-17 13:27:15 -0500

    17-Nov-19: Naresh : WebDriver methods

--- src/webdriver/scripts/WebDriverTest.java
+++ src/webdriver/scripts/WebDriverTest.java
@@ -1,5 +1,6 @@
 package webdriver.scripts;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,13 +8,34 @@ public class WebDriverTest {
 	
 	public static WebDriver driver;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Aruna Ravilla\\Documents\\GitHub\\Java_Selenium\\browsers\\chromedriver.exe" );
+	// Project Path
+		String strProjectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", strProjectPath + "\\browsers\\chromedriver.exe" );
 		
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://www.newtours.demoaut.com");
+		System.out.println(driver.getWindowHandle()); // Session ID
+		System.out.println(driver.getCurrentUrl()); // Page current URL
+		String strTitle = driver.getTitle();
+		System.out.println(strTitle);
+		driver.findElement(By.name("userName")).sendKeys("Aruna");
+		Thread.sleep(2000);
+		driver.findElement(By.name("password")).sendKeys("Secure1234");
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("REGISTER")).click(); // Selenium WebDriver code to click on a link
+		Thread.sleep(2000);
+		System.out.println(driver.getWindowHandle()); // Session ID
+		System.out.println(driver.getCurrentUrl()); // Page current URL
+		driver.navigate().back(); // Navigate the browser back
+		Thread.sleep(2000);
+		driver.navigate().forward(); // Navigate the browser forward
+		Thread.sleep(2000);
+		driver.navigate().refresh(); // Refresh the browser
+		System.out.println(driver.getWindowHandles()); // Session ID's of the tabs opened
+		driver.quit(); // Close the browser instances
 	}
 	
 
